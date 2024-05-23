7a263a9ba489ddf385bb86cad8e67aff3bf6084e
Cong Nhut <dgxa9999@gmail.com>
2024-05-23 17:02:21 +0700

    1

--- src/components/input-search/index.jsx
+++ src/components/input-search/index.jsx
@@ -1,9 +1,14 @@
 import { IoIosSearch } from 'react-icons/io';
 import css from './input-search.module.scss';
+import { useTheme } from 'src/context/dark-theme';
 
 function InputSearch() {
+	//theme
+	const { isDarkMode } = useTheme();
+	const darkClass = isDarkMode ? css.dark : '';
+
 	return (
-		<div className={css.inputSearch}>
+		<div className={`${css.inputSearch} ${darkClass}`}>
 			<IoIosSearch />
 			<input placeholder='Search' type="text" />
 		</div>
